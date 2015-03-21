55769ae7df80dc7d6612d32ab261a74d4fa015ee
Trenton Kennedy <trenton.kennedy@effectiveui.com>
2015-03-20 23:02:45 -0600

    create helper and loop through member cards

--- app/lib/view_helper.js
+++ app/lib/view_helper.js
@@ -4,3 +4,11 @@ Handlebars.registerHelper("include", function(name, context){
 
 	return new Handlebars.SafeString(template(context));
 });
+
+
+Handlebars.registerHelper('times', function(n, block) {
+	var accum = '';
+	for(var i = 0; i < n; ++i)
+		accum += block.fn(i);
+	return accum;
+});
