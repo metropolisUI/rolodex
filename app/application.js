6bcb46148e9ccd3898d69675598761c0ae3e3ad9
chadwithuhc <chadwithuhc@gmail.com>
2015-03-21 19:25:38 -0600

    Remove unneeded mixin

--- app/application.js
+++ app/application.js
@@ -2,13 +2,13 @@ require('lib/parseInit');
 var NavView = require('views/navView');
 
 // Application bootstrapper.
-var Application = _.extend({
+var Application = {
   initialize: function () {
     var Router = require('router');
     this.router = new Router();
 
     $('#app').prepend((new NavView()).$el);
   }
-}, Backbone.Events);
+};
 
 module.exports = Application;
