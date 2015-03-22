2963b95187b55e17fbf4ec08b391b2095db03c47
Trenton Kennedy <trenton.kennedy@effectiveui.com>
2015-03-22 00:23:45 -0600

    resolve merge conflicts

--- app/views/homeView.js
+++ app/views/homeView.js
@@ -9,18 +9,54 @@ module.exports = View.extend({
 
   initialize: function () {
     var promises = [];
-
     promises.push(topics.fetch().then(function (results) {
       this.data.topics = results.toJSON();
     }.bind(this)));
 
     promises.push(User.all().then(function (users) {
       this.data.users = users.toJSON();
+
     }.bind(this)));
 
     Parse.Promise.when(promises).then(function () {
-      this.render();
+
+	    this.render();
     }.bind(this));
-  }
+
+  },
+
+	afterRender: function() {
+		var searchResults = [],
+				userData = this.data.users;
+
+		for(var i = 0; i < userData.length; i++){
+			searchResults.push(userData[i].name, userData[i].email, userData[i].jobTitle);
+		}
+
+		var substringMatcher = function(strs) {
+			return function findMatches(q, cb) {
+				var matches, substrRegex;
+				matches = [];
+				substrRegex = new RegExp(q, 'i');
+				$.each(strs, function(i, str) {
+					if (substrRegex.test(str)) {
+						matches.push({ value: str });
+					}
+				});
+
+				cb(matches);
+			};
+		};
+
+		$('.typeahead').typeahead({
+					hint: true,
+					highlight: true,
+					minLength: 1
+				},
+				{
+					displayKey: 'value',
+					source: substringMatcher(searchResults)
+				});
+	}
 
 });
