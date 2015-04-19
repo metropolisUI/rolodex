1e087f9e9029936f36c3824eb0b8c3ad4a42ca08
Trenton Kennedy <trentontri@gmail.com>
2015-04-18 22:03:02 -0600

    set serverjs to render index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port',(process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render('index');
 });
 
 app.listen(app.get('port'), function(){
