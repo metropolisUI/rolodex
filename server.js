381a69db01498e7e3d0bccea0a0ed1af8e64cea6
Trenton Kennedy <trentontri@gmail.com>
2015-04-18 21:49:50 -0600

    add response for loading index.html

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 app.set('port',(process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response){
-  response.send('Hello World');
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(app.get('port'), function(){
