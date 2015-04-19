da522be2faffcc231b5d225a53aff1cc42404116
Trenton Kennedy <trentontri@gmail.com>
2015-04-18 22:06:12 -0600

    set view engine to html

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port',(process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'html');
 
 app.get('/', function(req, res){
   res.render('index');
