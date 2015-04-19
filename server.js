8b77ad67a423f899d4e620b6c432fd32f67c18ed
Trenton Kennedy <trentontri@gmail.com>
2015-04-18 22:20:20 -0600

    reset server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.set('port',(process.env.PORT || 3000));
-app.use(express.static('/public'));
-// app.set('views', '/public');
-// app.set('view engine', 'html');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.render('/public/index.html');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.listen(app.get('port'), function(){
-  console.log("app running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
