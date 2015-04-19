cd4526f34babea5beccb2b62c65fad38310e8d07
Trenton Kennedy <trentontri@gmail.com>
2015-04-18 22:13:22 -0600

    set html view engine

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set('port',(process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'html');
+app.use(express.static('/public'));
+// app.set('views', '/public');
+// app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('/public/index.html');
 });
 
 app.listen(app.get('port'), function(){
