fc35486716c28e07dc20777ac88e030229331cd6
Trenton <trentontri@gmail.com>
2015-11-28 21:06:51 -0700

    build out project splash page

--- app/components/SplashPage.jsx
+++ app/components/SplashPage.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 
 class SplashPage extends Component {
     render() {
-        return(
-            <h1>Hello Worldss</h1>
+        return (
+            <div className="header">
+                <nav className="nav site-nav">
+                    <div className="container">
+                        <ul className="nav nav-pills navbar-left">
+                            <li><a href="" className="logo">Rolodex.me</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Documentation</a></li>
+                        </ul>
+                        <ul className="nav nav-pills navbar-right">
+                            <li><a href="">Join</a></li>
+                            <li><a href="">Log In</a></li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="site-introduction">
+                    <h1>A searchable database of people you care about</h1>
+                </div>
+            </div>
         )
     }
 }
--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
                 <SplashPage />
             </div>
         )
