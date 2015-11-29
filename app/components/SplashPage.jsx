493a544b02ad354ce6e8e41f464a12854017cd4b
Trenton <trentontri@gmail.com>
2015-11-28 20:10:22 -0700

    create new component for product splash page

---
+++ app/components/SplashPage.jsx
@@ -0,0 +1,12 @@
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+
+class SplashPage extends Component {
+    render() {
+        return(
+            <h1>Hello Worldss</h1>
+        )
+    }
+}
+
+export default SplashPage;
